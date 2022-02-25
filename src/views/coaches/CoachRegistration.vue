5c433fe5b529cd017bbddfb3001375d0f172be05
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-02-26 00:17:20 +0200

    Create basic registration form

---
+++ src/views/coaches/CoachRegistration.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form />
+    </base-card>
+  </section>
+</template>
+
+<script>
+import CoachForm from "../../components/coaches/CoachForm.vue"
+
+export default {
+  components: {
+    "coach-form": CoachForm,
+  },
+}
+</script>
